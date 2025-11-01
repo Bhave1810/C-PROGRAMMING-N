4469f66186923f95ec3037adf8c68b79a37ab82c
Bhave1810 <Engineer@1845>
2025-11-02 00:35:00 +0530

    update

--- day-1/sum.c
+++ day-1/sum.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
-int main(void) {
-    double num1, num2, sum;
-    printf("Enter two numbers: ");
-    if (scanf("%lf %lf", &num1, &num2) != 2) {
-        fprintf(stderr, "Invalid input. Please enter two numbers.\n");
-        return 1;
-    }
-    sum = num1 + num2;
-    printf("Sum = %g\n", sum);
-    return 0;
-}/* Simple program to read two numbers and print their sum */
+# include <stdio.h>
+int main (){
+    int a,b;
+    printf("Enter First Name ");
+    scanf("%d",&a);
+    printf("Enter First Name ");
+    scanf("%d",&b);
+
+    if a>b ?printf(a):printf(b);
+}
